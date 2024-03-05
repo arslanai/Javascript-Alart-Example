bdcd8a55eb752a9e349f80815f4046c160a5cc20
Arslan <muhammadarslanrajput12@gmail.com>
2024-03-05 08:55:54 -0800

    Javascript alart

---
+++ Javascript/app.js
@@ -0,0 +1,13 @@
+let userName = prompt('What is your name?')
+let fatherName = prompt('What is your Father Name?')
+let contactNumber = prompt('Your Type Contact Number')
+
+
+document.write('<br /> <span class="contant"> Welcome to <span class="contaner">' + userName + ' </span> Web page </span>');
+document.write("<br />");
+document.write("<br />");
+document.write('<br /> <span class="fname">Welcom to <span class="mid">'+fatherName+'</span>shahab web page</span>')
+document.write("<br />");
+document.write("<br />");
+document.write('<br /><span class="conNumber">Thankyou <span class="main">'+contactNumber+'</span></span>')
+
